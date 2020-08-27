c69c89f73a61f56d6413f2f52f08de253c16c6d3
mihir marathe <mihir9310@gmail.com>
2020-08-27 13:56:37 +0530

    minor correction

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -83,7 +83,7 @@ function ButtonGroup(props) {
 
             console.log(data);
             tempPaymentArray[i].status = data.statuscode;
-            tempPaymentArray[i].error = data.statusError;
+            tempPaymentArray[i].statusError = data.error;
 
             var filteredCurrentEnrty = props.paymentList.filter(function (el) {
               return el.uniqueRequestNo !== tempPaymentArray[i].uniqueRequestNo;
