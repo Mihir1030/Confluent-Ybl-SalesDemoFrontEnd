6198bb5e6a7b0b97a343171c5899b0b5dd4e1556
mihir marathe <mihir9310@gmail.com>
2020-09-09 00:54:47 +0530

    eslint corrections

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,31 +1,54 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import BootStrapButton from "react-bootstrap/Button";
 import Popover from "react-bootstrap/Popover";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 function Button(props) {
-  const badge = props.badge;
+  const {
+    badge,
+    popoverTitle,
+    popoverContent,
+    popoverPlacement,
+    variant,
+    buttonClick,
+    text,
+  } = props;
 
   const popover = (
     <Popover id="popover-basic">
-      <Popover.Title as="h3">{props.popoverTitle}</Popover.Title>
-      <Popover.Content>{props.popoverContent}</Popover.Content>
+      <Popover.Title as="h3">{popoverTitle}</Popover.Title>
+      <Popover.Content>{popoverContent}</Popover.Content>
     </Popover>
   );
 
   return (
     <OverlayTrigger
-      trigger="hover"
-      placement={props.popoverPlacement}
+      // trigger="hover"
+      placement={popoverPlacement}
       overlay={popover}
     >
-      <BootStrapButton variant={props.variant} onClick={props.buttonClick}>
-        {props.text}
+      <BootStrapButton variant={variant} onClick={buttonClick}>
+        {text}
         {badge}
       </BootStrapButton>
     </OverlayTrigger>
   );
 }
+
+Button.propTypes = {
+  badge: PropTypes.element,
+  popoverTitle: PropTypes.string.isRequired,
+  popoverContent: PropTypes.element.isRequired,
+  popoverPlacement: PropTypes.string.isRequired,
+  variant: PropTypes.string.isRequired,
+  buttonClick: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+Button.defaultProps = {
+  badge: null,
+};
 
 export default Button;
