cc0e3eb02c9bad081751354eae26e7ff1a2fd68e
mihir marathe <mihir9310@gmail.com>
2020-09-09 11:10:01 +0530

    eslint corrections

--- src/components/PaymentTable.js
+++ src/components/PaymentTable.js
@@ -1,10 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+import uuid from "react-uuid";
 
 import GridComponent from "./GridComponent";
 import Heading from "./HeadingComponent";
 
 function PaymentTable(props) {
   console.log("pay table run");
+
+  const { paymentList } = props;
+
   const heading = [
     "Unique request number",
     "Name",
@@ -16,19 +22,21 @@ function PaymentTable(props) {
     "Unique Refrence Number",
     "Error",
   ];
-  const tableHeadingList = heading.map((h, index) => <th key={index}>{h}</th>);
+  const tableHeadingList = heading.map((headingText) => (
+    <th key={uuid()}>{headingText}</th>
+  ));
 
-  const entryRows = props.paymentList.map((e, index) => (
-    <tr key={index}>
-      <td>{e.uniqueRequestNo}</td>
-      <td>{e.beneficiaryName}</td>
-      <td>{e.beneficiaryAddress}</td>
-      <td>{e.beneficiaryBankIfsc}</td>
-      <td>{e.beneficiaryAccountNumber}</td>
-      <td>{e.transferAmount}</td>
-      <td>{e.transferType}</td>
-      <td>{e.uniqueRefrenceNumber}</td>
-      <td>{e.error}</td>
+  const entryRows = paymentList.map((entry) => (
+    <tr key={uuid()}>
+      <td>{entry.uniqueRequestNo}</td>
+      <td>{entry.beneficiaryName}</td>
+      <td>{entry.beneficiaryAddress}</td>
+      <td>{entry.beneficiaryBankIfsc}</td>
+      <td>{entry.beneficiaryAccountNumber}</td>
+      <td>{entry.transferAmount}</td>
+      <td>{entry.transferType}</td>
+      <td>{entry.uniqueRefrenceNumber}</td>
+      <td>{entry.error}</td>
     </tr>
   ));
 
@@ -46,13 +54,17 @@ function PaymentTable(props) {
   );
 }
 
+PaymentTable.propTypes = {
+  paymentList: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
 function areEqual(prevProps, nextProps) {
   /*
   return true if passing nextProps to render would return
   the same result as passing prevProps to render,
   otherwise return false
   */
-  return prevProps.paymentList === nextProps.paymentList ? true : false;
+  return prevProps.paymentList === nextProps.paymentList;
 }
 
 export default React.memo(PaymentTable, areEqual);
