ae3bd5632ed54312716fde3be7064dc0daf7e13f
mihir marathe <mihir9310@gmail.com>
2020-09-09 11:08:41 +0530

    eslint corrections

--- src/components/GridComponent.js
+++ src/components/GridComponent.js
@@ -1,17 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import BootstrapTable from "react-bootstrap/Table";
 
 function GridComponent(props) {
+  const { tableHeadingList, entryRows } = props;
   return (
     <div className="tableDiv">
-      <BootstrapTable striped bordered hover size="sm" responsive={true}>
+      <BootstrapTable striped bordered hover size="sm" responsive>
         <thead>
-          <tr>{props.tableHeadingList}</tr>
+          <tr>{tableHeadingList}</tr>
         </thead>
-        <tbody>{props.entryRows}</tbody>
+        <tbody>{entryRows}</tbody>
       </BootstrapTable>
     </div>
   );
 }
+
+GridComponent.propTypes = {
+  tableHeadingList: PropTypes.arrayOf(PropTypes.element).isRequired,
+  entryRows: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default GridComponent;
