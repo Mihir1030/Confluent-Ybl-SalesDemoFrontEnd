618df8e217a4971b98287ce94fdc47e975e788e7
mihir marathe <mihir9310@gmail.com>
2020-09-10 13:52:38 +0530

    eslint correction for file

--- src/components/AlertDismissible.js
+++ src/components/AlertDismissible.js
@@ -1,18 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import Alert from "react-bootstrap/Alert";
 
 function AlertDismissible(props) {
+  const { setShowAlert, alertMessage } = props;
   return (
-    <Alert
-      variant="danger"
-      onClose={() => props.setShowAlert(false)}
-      dismissible
-    >
+    <Alert variant="danger" onClose={() => setShowAlert(false)} dismissible>
       <Alert.Heading>Oops! Something went wrong</Alert.Heading>
-      <p>{props.alertMessage}</p>
+      <p>{alertMessage}</p>
     </Alert>
   );
 }
+
+AlertDismissible.propTypes = {
+  setShowAlert: PropTypes.bool.isRequired,
+  alertMessage: PropTypes.string.isRequired,
+};
 
 export default AlertDismissible;
