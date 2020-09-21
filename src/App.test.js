02b502acef78266866f39681164e029dabb89c1d
mihir marathe <mihir9310@gmail.com>
2020-09-21 14:13:34 +0530

    updated testes

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Payment Table/i);
+  const linkElement = getByText(/Payments Table/i);
   expect(linkElement).toBeInTheDocument();
 });
